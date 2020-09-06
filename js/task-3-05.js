de221386acb3b73b5a1e93afe80437543a5a6a86
Pavlenko-Oksana <65184570+Pavlenko-Oksana@users.noreply.github.com>
2020-09-06 20:23:35 +0300

    добавляет решение задачи 3

---
+++ js/task-3-05.js
@@ -0,0 +1,57 @@
+// Write code under this line
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      const good = this.items[i];
+
+      if (good === item) {
+        this.items.splice(i, 1);
+      }
+    }
+  }
+}
+console.log(typeof Storage);
+// 'function'
+
+const goods = ["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор"];
+
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem("Дроид");
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem("Пролонгер");
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
